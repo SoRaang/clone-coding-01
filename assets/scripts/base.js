a9e78ff730793ef4c0ccde2d3fcf683174b936d5
SoRaang <dsc0320@hotmail.com>
2024-08-06 17:40:50 +0900

    Save

---
+++ assets/scripts/base.js
@@ -0,0 +1,8 @@
+const searchToggler = document.querySelector('.search-toggle');
+
+searchToggler.addEventListener('click', toggleAction);
+
+function toggleAction() {
+    if (!searchToggler.classList.contains('on')) searchToggler.classList.toggle('on');
+    searchToggler.removeEventListener('click', toggleAction);
+}
