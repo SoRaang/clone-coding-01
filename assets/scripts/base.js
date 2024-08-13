ee9b0e6373a9b1f53be220a0ee19dfcd535e05b6
SoRaang <dsc0320@hotmail.com>
2024-08-13 11:32:37 +0900

    Save

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -10,3 +10,38 @@ function toggleAction() {
 const navObject = [
 
 ];
+
+// ----- 뉴스티커
+
+const newsScroller = document.getElementById('newsScroller');
+
+const newsTicker = setInterval(() => {
+    const newsItem = newsScroller.querySelector('li:first-child');
+
+    newsScroller.appendChild(newsItem);
+}, 3000)
+
+// ----- 프로모션 슬라이더
+
+const promoOpener = document.querySelector('.promotion-opener');
+const promoSlider = document.getElementById('promoSlider');
+
+promoOpener.addEventListener('click', () => promoSlider.classList.toggle('on'));
+
+const sliderContainer = document.querySelector('.slider-container');
+const btnPrev = document.getElementById('btnPrev');
+const btnNext = document.getElementById('btnNext');
+const slidePagination = document.getElementById('slidePagination');
+const slider = new Swiper(sliderContainer, {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    navigation: {
+        prevEl: btnPrev,
+        nextEl: btnNext
+    },
+    pagination: {
+        el: slidePagination
+    },
+    centeredSlides: true
+});
